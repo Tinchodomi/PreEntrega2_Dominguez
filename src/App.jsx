584d0493ebc8fa92fe392f321cd9e2336ebcbebb
Tinchodomi <tinchodomi@gmail.com>
2023-05-24 12:12:19 -0300

    PreEntrega2Dominguez

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,18 @@ function App() {
   return (
     <>
       <BrowserRouter>
-          <Navbar/>
-          <div>
-            <img
-              className="fondo"
-              src="https://nikearprod.vtexassets.com/assets/vtex.file-manager-graphql/images/34650025-e6ae-4bdb-8791-2b4b3f9ffbed___a557edc0961bf27cd51ecfcaca73828e.png"
-              alt=""
-            />
-          </div>
-          <Routes>
-            <Route path="/" element={<ItemListContainer />} />
-            <Route
-              path="/categoria/:idCategoria"
-              element={<ItemListContainer />}
-            />
-            <Route path="/item/:idItem" element={<ItemDetailContainer />} />
-            <Route path="/" element={<ItemListContainer />} />
-            <Route path="*" />
-          </Routes>
-          <ModoOscuro/>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route
+            path="/categoria/:idCategoria"
+            element={<ItemListContainer />}
+          />
+          <Route path="/item/:idItem" element={<ItemDetailContainer />} />
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="*" />
+        </Routes>
+        <ModoOscuro/>
       </BrowserRouter>
     </>
   );
--- src/TiendaNike/Navbar/Navbar.jsx
+++ src/TiendaNike/Navbar/Navbar.jsx
@@ -5,37 +5,43 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className="header">
-      <Link to={"/"} className="link_logo">
-        <Logo/>
-      </Link>
-      <nav>
-        <ul className="ul">
-          <li className="li">
-            <Link className="link" to={"/item/1"}>
-              Hombre
-            </Link>
-          </li>
-          <li className="li">
-            <Link className="link" to={"/item/2"}>
-              Mujer
-            </Link>
-          </li>
-          <li className="li">
-            <Link className="link" to={"/item/3"}>
-              Niño/a
-            </Link>
-          </li>
-          <li className="li">
-            <Link className="link">
-             Destacados
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Car />
-      
-    </header>
+    <>
+      <header className="header">
+        <Link to={"/"} className="link_logo">
+          <Logo />
+        </Link>
+        <nav>
+          <ul className="ul">
+            <li className="li">
+              <Link className="link" to={"/item/1"}>
+                Hombre
+              </Link>
+            </li>
+            <li className="li">
+              <Link className="link" to={"/item/2"}>
+                Mujer
+              </Link>
+            </li>
+            <li className="li">
+              <Link className="link" to={"/item/3"}>
+                Niño/a
+              </Link>
+            </li>
+            <li className="li">
+              <Link className="link">Destacados</Link>
+            </li>
+          </ul>
+        </nav>
+        <Car />
+      </header>
+      <section>
+        <img
+          className="fondo"
+          src="https://nikearprod.vtexassets.com/assets/vtex.file-manager-graphql/images/34650025-e6ae-4bdb-8791-2b4b3f9ffbed___a557edc0961bf27cd51ecfcaca73828e.png"
+          alt=""
+        />
+      </section>
+    </>
   );
 };
 
